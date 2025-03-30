167306432f3192721b890af66cbbd609122a8b48
Viraj jadhav <JADHAVVIRAJ043@GMAIL.COM>
2025-03-30 20:20:34 +0530

    Add client/src/App.js 1743346328818

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import './App.css';
 
 // Pages
 import Home from './pages/Home';
-import Login from './pages/Login';
+import LoginNew from './pages/LoginNew';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Jobs from './pages/Jobs';
 import Messages from './pages/Messages';
+import JobDetails from './pages/JobDetails';
 
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import JobList from './components/JobList';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/messages" element={<Messages />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<LoginNew />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/jobs" element={<Jobs><JobList /></Jobs>} />
+              <Route path="/jobs/:id" element={<JobDetails />} />
+              <Route path="/messages" element={<Messages />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
